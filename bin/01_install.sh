f167ff6268643ff48d79a7d15746172a47b499bf
devcon <devcon@admantium.com>
2022-10-22 12:28:03 +0200

    Update: KUBERNETES_VERSION is env var

--- bin/01_install.sh
+++ bin/01_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 01 ContainerD Insallation & Setup
+# 01 containerd installaation and configuration
 apt update
 apt install apparmor apparmor-utils containerd -y
 
-cat <<EOF |sudo tee /etc/containerd/config.toml
+cat <<EOF > /etc/containerd/config.toml
 version = 2
 [plugins]
   [plugins."io.containerd.grpc.v1.cri"]
@@ -18,16 +18,20 @@ EOF
 
 systemctl restart containerd
 
-# 02 Kubernetes Installation
+# 02 kubernetes component installation
+export KUBERNETES_VERSION=1.25.2
 
 apt install -y apt-transport-https ca-certificates curl
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt update
-apt install -y kubeadm=1.23.11-00 kubectl=1.23.11-00 kubelet=1.23.11-00
+apt install -y \
+   kubeadm=$KUBERNETES_VERSION-00 \
+   kubectl=$KUBERNETES_VERSION-00 \
+   kubelet=$KUBERNETES_VERSION-00
 
-# 03 Configure Network
-cat <<EOF | tee /etc/modules-load.d/k8s.conf
+# 03 network configuration
+cat <<EOF > /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
@@ -35,7 +39,7 @@ EOF
 modprobe overlay
 modprobe br_netfilter
 
-> cat <<EOF | tee /etc/sysctl.d/k8s.conf
+cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
--- bin/02_kubeadm_init.sh
+++ bin/02_kubeadm_init.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# 01 Init Cluster
+# 01 init cluster
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 kubeadm init \
   --cri-socket /run/containerd/containerd.sock \
   --pod-network-cidr=192.168.0.0/16
 
-# 02 Copy kubeadm
-
+# 02 copy kubeconfig
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-# 03 Install Network Plugin
+# 03 install cni
 
 kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/tigera-operator.yaml
 sleep 5 #wait for the deployment to start the required pods
